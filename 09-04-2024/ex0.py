1c23553dd81e294ee65cc92f18265ffdbf6eccb0
Lucas270206 <165733805+Lucas270206@users.noreply.github.com>
2024-04-09 21:57:29 -0300

    Update ex0.py
    joquempo

--- 09-04-2024/ex0.py
+++ 09-04-2024/ex0.py
@@ -7,21 +7,31 @@ continuar = 0
 
 print("Então você quer jogar um pedra, papel e tesoura.... \n então bora!!!")
 modalidade = int(input('Para começar você deve escolher uma modalidade irmão escreva 1 para Humano x Humano; 2 para Humano x Maquina e 3 para Maquina x maquina: '))
+while modalidade != 1 and modalidade != 2 and modalidade != 3:
+    modalidade = int(input('Não sabe ler escolha uma modalidade VALIDA irmão escreva 1 para Humano x Humano; 2 para Humano x Maquina e 3 para Maquina x maquina: '))
 while continuar != 2:
     if modalidade == 1:
         print("----" *25)
         num1 = int(input("player 1 - Escolha: 1 = pedra / 2 = papel / 3 = tesoura "))
         num2 = int(input("player 2 - Escolha: 1 = pedra / 2 = papel / 3 = tesoura "))
+        while num1 != 1 and num1 != 2 and num1 != 3 and num2 != 1 and num2 != 2 and num2 != 3:
+            num1 = int(input("player 1 - Escolha: 1 = pedra / 2 = papel / 3 = tesoura "))
+            num2 = int(input("player 2 - Escolha: 1 = pedra / 2 = papel / 3 = tesoura "))
+
     elif modalidade == 2:
         print("----" *25)
         num1 = int(input("player 1 - Escolha: 1 = pedra / 2 = papel / 3 = tesoura ")) 
         num2 = random.randint(1,3)
         print("----" *25)
+        while num1 != 1 and num1 != 2 and num1 != 3:
+            num1 = int(input("player 1 - Escolha: 1 = pedra / 2 = papel / 3 = tesoura "))
     else:
         print("----" *25)
         num1 = random.randint(1,3)
         num2 = random.randint(1,3)
         print("----" *25)
+
+
     if num1 == 1 and num2 == 1:
         print('player 1 escolheu pedra \nPlayer 2 escolheu pedra \nDeu empate meu amores =)')
         empate += 1 
